f2ad6cd0890cd8295a89213c7a7d8ff62afa7316
PRAJNANANDA1234 <sonupinky393@gmail.com>
2023-05-15 08:54:13 -0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from app.form import *
+from django.http import HttpResponse
+# Create your views here.
+class cbv_context(TemplateView):
+    template_name='cbv_context.html'
+
+    def get_context_data(self, **kwargs) :
+            EDCO=super().get_context_data(**kwargs)
+            EDCO['name']='PRAJNA'
+            EDCO['age']=24
+            return EDCO
+class cbv_form(TemplateView):
+      template_name='cbv_form.html'
+
+      def get_context_data(self, **kwargs):
+            EDCO=super().get_context_data(**kwargs)
+            SO=Studentform()
+            EDCO['SO']=SO
+            return EDCO
+      def post(self,request):
+            SOF=Studentform(request.POST)
+            if SOF.is_valid():
+                  SOF.save()
+            return HttpResponse('insertion successfully')
+
+    
+    
